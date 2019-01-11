dd0231b3a7113447e30011e465e9f5dd0008100a
Bruno Dolce <brudolce@gmail.com>
2019-01-11 14:54:17 -0300

    finished 11/01/2019

---
+++ Thinkful-Drills:Repeater.js
@@ -0,0 +1,13 @@
+//Write a class function named repeat() that takes
+//two arguments (a string and a long integer), and 
+//returns a new string where the input string is 
+//repeated that many times. For example:
+
+function repeater(string, n){
+    newa = ""
+    for(i=1;i<=n;i+=1){
+        newa += string
+    }
+    return newa;
+  }
+
---
+++ Tribonacci Sequence.js
@@ -0,0 +1,10 @@
+function tribonacci(signature,n){
+    if (n < 3) {
+      return signature.slice(0,n);
+    }
+    let arr = signature;  
+    for (var i = 3; i < n; i++) {
+      arr.push((arr[i-1] + arr[i-2] + arr[i-3]));
+    }
+    return arr;
+}
