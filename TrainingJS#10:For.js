c71cd09ce79b622ca75112f0094c8cdb21235e00
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-01-14 21:16:34 -0300

    Add files via upload

---
+++ TrainingJS#10:For.js
@@ -0,0 +1,9 @@
+function pickIt(arr){
+    var odd=[],even=[];
+    for(let i=0;i<arr.length;i+=1){
+      if(arr[i]%2===0){even.push(arr[i])
+      } else {odd.push(arr[i])
+      }
+    }
+    return [odd,even];
+  }
