fd66130cf0c54248143826cec306f6e1866e860f
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-01-23 08:47:40 -0300

    Create Digital cypher.js

---
+++ Digital cypher.js
@@ -0,0 +1,10 @@
+function encode(str,  n){
+  const str0 = String(n);
+  let arr=[];
+  for(i=0;i<str.length;i+=1){
+    let a = parseInt(str0[i % str0.length]);
+    let b = (str[i].charCodeAt() - 96)
+    arr.push(a + b)
+  }
+    return arr;
+}
