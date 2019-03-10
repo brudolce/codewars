4de7ed3e4c8020a6ec4afa39dc37fd777789f13b
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-10 18:13:11 -0300

    done

---
+++ 6-kyu/Help the bookseller !.py
@@ -0,0 +1,16 @@
+def stock_list(listOfArt, listOfCat):
+    if len(listOfCat) == 0 or len(listOfCat) == 0:
+        return ''
+    ans = [int(i) for i in list(str(0)*len(listOfCat))]
+    for i in listOfArt:
+        i = i.split()
+        if i[0][0] in listOfCat:
+            ans[listOfCat.index(i[0][0])] += int(i[1])
+    if ans.count(0) == len(ans):
+        return ''
+    ans_arr = []
+    i = 0
+    while i < len(listOfCat):
+        ans_arr.append('(' + listOfCat[i] + ' : ' + str(ans[i]) + ')')
+        i += 1
+    return ' - '.join(ans_arr)
