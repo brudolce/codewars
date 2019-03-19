dc4250b142f928ab04bb73a2061ca4b0e803f166
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-18 21:30:53 -0300

    done

--- 5-kyu/Part 1: Evil Programming Government Restrictions.js
+++ 5-kyu/Part 1: Evil Programming Government Restrictions.js
@@ -2,3 +2,13 @@ const anarchy = function () {
   return
  'I can write numbers like, ' + +!!true + ', ' + (+!!true + (+!!true)) + ', ' + (+!!true + (+!!true)+ (+!!true)) + '.';
 }
+
+
+// clever stuff
+
+let anarchy = function () {
+  var one = Math.PI / Math.PI;
+  var two = one + one;
+  var three = two + one;
+  return 'I can write numbers like, ' + one + ', ' + two + ', ' + three + '.';
+}
