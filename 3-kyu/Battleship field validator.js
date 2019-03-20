f7a19d1c8787f4757588ae70fa00bc7370e78efd
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-20 20:41:41 -0300

    seems like sudoku solver

---
+++ 3-kyu/Battleship field validator.js
@@ -0,0 +1,49 @@
+function validateBattlefield(field) {
+  let ships = {};
+  let len = 0; // length of each ship
+  let direction; // dfs direction
+  let invalid = false; // check for dfs direction changing => ship contact
+  function dfs(i, j, field, dir) {
+    if (i < 0 || j < 0 || i >= field.length || j >= field[0].length || field[i][j] != '1')
+      return;
+    if (dir != direction)
+      invalid = true;
+    len += 1;
+    field[i][j] = 2; // visited
+    dfs(i-1, j, field, 'u');
+    dfs(i+1, j, field, 'd');
+    dfs(i, j-1, field, 'l');
+    dfs(i, j+1, field, 'r');
+    dfs(i-1, j-1, field, 'ul'); // check for contact
+    dfs(i-1, j+1, field, 'ur'); // check for contact
+    dfs(i+1, j-1, field, 'dl'); // check for contact
+    dfs(i+1, j+1, field, 'dr'); // check for contact
+  }
+  for (let i = 0; i < field.length; i += 1) {
+    for (let j = 0; j < field[0].length; j += 1) {
+      if (field[i][j] == 1) {
+        direction = field[i][j+1] == 1 ? 'r' : 'd';
+        dfs(i, j, field, direction);
+        if (invalid == true)
+          return false;
+      }
+      if (len > 0) {
+        if (!(len in ships))
+          ships[len] = 0;
+        ships[len] += 1;
+      }
+      len = 0;
+    }
+  }
+  
+  for (let i = 1; i <= 4; i+= 1) {
+    let k = i.toString();
+    if (!(k in ships) || ships[k] != 4 - i + 1) return false;
+    delete ships[k];
+  }
+  
+  // if there is any ship remaining
+  for (let k in ships) return false;
+  
+  return true;
+}
