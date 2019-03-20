e2e055d1b996a72ee92c161022b0aeedeec07ffa
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-20 16:52:19 -0300

    added clever solutions

--- 5-kyu/Can you get the loop ?.js
+++ 5-kyu/Can you get the loop ?.js
@@ -14,3 +14,34 @@ function loop_size(node){
   }
   return len;
 }
+
+//clever ludic solution
+function loop_size(node){
+  var turtle = node;
+  var rabbit = node;
+  
+  // Find a point in the loop.  Any point will do!
+  // Since the rabbit moves faster than the turtle
+  // and the kata guarantees a loop, the rabbit will
+  // eventually catch up with the turtle.
+  do {
+    turtle = turtle.getNext();
+    rabbit = rabbit.getNext().getNext();
+  }
+  while (turtle != rabbit);
+  
+  // The turtle and rabbit are now on the same node,
+  // but we know that node is in a loop.  So now we
+  // keep the turtle motionless and move the rabbit
+  // until it finds the turtle again, counting the
+  // nodes the rabbit visits in the mean time.
+  var count = 0;
+  do {
+    ++count;
+    rabbit = rabbit.getNext();
+  }
+  while (turtle != rabbit);
+
+  // voila
+  return count;
+}
