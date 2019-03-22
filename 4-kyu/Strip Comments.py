e80617280b836512c8b863bdb0e0551118312096
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-22 13:17:50 -0300

    done

---
+++ 4-kyu/Strip Comments.py
@@ -0,0 +1,11 @@
+def solution(string,markers):
+    
+    stringSplit = string.split('\n')
+
+    def stripSentence(s):            
+        afterStrip = s
+        for m in markers:
+            if m in s and len(afterStrip) > len(s[:s.find(m)].rstrip()):
+                afterStrip = s[:s.find(m)].rstrip()
+        return afterStrip
+    return '\n'.join([stripSentence(s) for s in stringSplit])
