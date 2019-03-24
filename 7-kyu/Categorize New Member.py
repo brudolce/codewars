a75793eb606797811adae1468c40287141a77881
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-24 16:56:06 -0300

    added clever solutions

--- 7-kyu/Categorize New Member.py
+++ 7-kyu/Categorize New Member.py
@@ -5,3 +5,17 @@ def filters(x):
 
 def openOrSenior(data):
     return list(map(filters, data))
+
+
+# clever solution 1
+def openOrSenior(data):
+  return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
+
+# clever solution 2
+    res = []
+    for i in data:
+      if i[0] >= 55 and i[1] > 7:
+        res.append("Senior")
+      else:
+        res.append("Open")
+    return res
