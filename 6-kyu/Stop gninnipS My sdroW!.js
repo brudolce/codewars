21ca1035723bfc72764905d784bd92852a599fa0
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-24 20:42:40 -0300

    done

--- 6-kyu/Stop gninnipS My sdroW!.js
+++ 6-kyu/Stop gninnipS My sdroW!.js
@@ -10,3 +10,9 @@ function spinWords(s){
   }
   return result.join(' ')
 }
+
+//clever solution
+
+function spinWords(string){
+  return string.replace(/\w{5,}/g, function(w) { return w.split('').reverse().join('') })
+}
