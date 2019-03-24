3485e78f208aa563cb412c970f05105d9c815b77
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-24 19:10:01 -0300

    done

---
+++ 8-kyu/Basic Mathematical Operations.js
@@ -0,0 +1,8 @@
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case '+': return value1 + value2;
+    case '-': return value1 - value2;
+    case '*': return value1 * value2;
+    case '/': return value1 / value2;
+  }
+}
