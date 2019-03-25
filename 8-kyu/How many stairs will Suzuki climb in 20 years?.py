dc7b4be047a67679156fd22901f80ad45e63ed12
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-25 14:42:29 -0300

    done

--- 8-kyu/How many stairs will Suzuki climb in 20 years?.py
+++ 8-kyu/How many stairs will Suzuki climb in 20 years?.py
@@ -5,3 +5,7 @@ def stairs_in_20(stairs):
     for i in stairs:
          result += reduce(lambda a,b : a + b, i)
     return result * 20
+
+#clever solution
+def stairs_in_20(stairs):
+    return 20 * sum(map(sum, stairs))
