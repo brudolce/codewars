60274de0d4ffee9a409720b46b1c42a8551482ef
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-26 22:27:04 -0300

    done

---
+++ 7-kyu/Make a function that does arithmetic!.js
@@ -0,0 +1,6 @@
+function arithmetic(a, b, operator){
+ if (operator === 'add') return a+b
+ if (operator === 'subtract') return a-b
+ if (operator === 'multiply') return a*b
+ if (operator === 'divide') return a/b
+}
