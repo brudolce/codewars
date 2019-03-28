acbe78e076113fc8e5cd75d3f52a20fc6493a444
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-28 19:50:53 -0300

    done

---
+++ 6-kyu/Convert string to camel case.js
@@ -0,0 +1,10 @@
+function toCamelCase(str){
+  str = str.split('');
+  return str.map(function(el, i){
+    if(el == '-' || el == '_'){
+      el = str[i+1].toUpperCase();
+      str.splice(i+1, 1);
+    }
+    return el;
+  }).join('');
+}
