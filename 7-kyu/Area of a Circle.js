872b1d709348b9453b57480929cca7b3cf359ef2
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-03-30 00:00:32 -0300

    done

--- 7-kyu/Area of a Circle.js
+++ 7-kyu/Area of a Circle.js
@@ -1,4 +1,3 @@
 const circleArea = radius =>
   radius <= 0 || typeof radius !== 'number'
-    ? false
-    : Number((Math.PI * Math.pow(radius, 2)).toFixed(2))
+    ? false : Number((Math.PI * Math.pow(radius, 2)).toFixed(2))
