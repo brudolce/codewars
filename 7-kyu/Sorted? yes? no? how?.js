44ed571944d755b93e32705cddfa7d6f33bdf194
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-01 11:04:50 -0300

    added clever solution

--- 7-kyu/Sorted? yes? no? how?.js
+++ 7-kyu/Sorted? yes? no? how?.js
@@ -3,3 +3,9 @@ function isSortedAndHow(array) {
   if (array.join('') === array.slice().sort((a,b)=>b-a).join('')) return 'yes, descending';
   return 'no'
 }
+
+//clever solution
+function isSortedAndHow(arr) {
+  return arr.every((x,i)=>i==0||arr[i]>=arr[i-1])?'yes, ascending':
+         arr.every((x,i)=>i==0||arr[i]<=arr[i-1])?'yes, descending':'no'
+}
