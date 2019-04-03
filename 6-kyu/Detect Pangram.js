962a2b86c98baf0f0fc6c6afc0e136a542d50091
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-03 11:35:38 -0300

    added clever solution

--- 6-kyu/Detect Pangram.js
+++ 6-kyu/Detect Pangram.js
@@ -2,3 +2,11 @@ function isPangram(string){
   let str=string.split(' ').join('').replace(/[\W\d]/g,'').toLowerCase().split('')
   return [...new Set(str)].length==26
 }
+
+//clever solution
+function isPangram(string){
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+    return string.indexOf(x) !== -1;
+  });
+}
