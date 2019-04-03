9e00a350aae5303cff133bead944385c8d1c9a16
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-03 10:07:28 -0300

    done

--- 8-kyu/String cleaning.py
+++ 8-kyu/String cleaning.py
@@ -4,3 +4,9 @@ def string_clean(s):
         if x not in ['0','1','2', '3', '4', '5', '6', '7', '8', '9']:
             l.append(x)
     return '' if l==[] else "".join(l) 
+
+
+# refactored
+
+def string_clean(s):
+    return ''.join(x for x in s if not x.isdigit())
