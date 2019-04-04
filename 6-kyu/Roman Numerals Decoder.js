b6bd8a12d75501ea5db8abcc2967ed84f839852c
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-04 15:51:34 -0300

    done

---
+++ 6-kyu/Roman Numerals Decoder.js
@@ -0,0 +1,4 @@
+function solution(roman){
+   const hashConvert = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+   return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((accum, roman) => accum + hashConvert[roman], 0);
+}
