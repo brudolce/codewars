b7f78ddd55711972fa9e0bf0b921042dacaa1a5d
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-08 14:41:16 -0300

    just like valid braces

--- 6-kyu/Checking Groups.js
+++ 6-kyu/Checking Groups.js
@@ -1,14 +1,14 @@
  function groupCheck(s){
   const pile =[]
   const obj = {
-    ')':'(',
-    ']':'[',
-    '}':'{'
+    '(':')',
+    '[':']',
+    '{':'}'
   }
   
   for (let i of s) {
     if (i === '(' || i === '[' || i === '{') pile.push(i);
-    else if (pile[pile.length-1] === obj[i]) pile.pop();
+    else if (pile[pile.length-1] === i]) pile.pop();
     else return false;
   }
     
