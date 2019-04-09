7f5af7876dc29af9ccfb8032d9455f079c53e922
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-09 15:54:44 -0300

    not finished-needs to be faster

--- 3-kyu/Alphabetic Anagrams.js
+++ 3-kyu/Alphabetic Anagrams.js
@@ -3,26 +3,18 @@ function listPosition(word) {
   word = word.split('');
   word = word.sort();
   let used = [];
-  for (let i = 0; i < word.length; i++)
-    used.push(false);
+  for (let i = 0; i < word.length; i++) used.push(false);
   let num = 0, flag = 0;
-  function dfs(path, word, w)
-  {
-    if (path.length == word.length)
-    {
+  function dfs(path, word, w) {
+    if (path.length == word.length) {
       num += 1;
-      if (path.join('') == w)
-        flag = 1;
+      if (path.join('') == w) flag = 1;
       return;
     }
-    for (let i = 0; i < word.length; i++)
-    {
-      if (flag == 0)
-      {
-        if (i > 0 && !used[i-1] && word[i] == word[i-1])
-          continue;
-        if (!used[i])
-        {
+    for (let i = 0; i < word.length; i++) {
+      if (flag == 0) {
+        if (i > 0 && !used[i-1] && word[i] == word[i-1]) continue;
+        if (!used[i]){
           used[i] = true;
           path.push(word[i]);
           dfs(path, word, w);
@@ -30,8 +22,7 @@ function listPosition(word) {
           used[i] = false;
         }
       }
-      else
-        break;
+      else break;
     }
   }
   dfs([], word, w);
