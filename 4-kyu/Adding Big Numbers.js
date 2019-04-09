cea89db260a1d185e9f79e860c97ce6ea09f9e9f
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-09 14:24:47 -0300

    added clever solution

--- 4-kyu/Adding Big Numbers.js
+++ 4-kyu/Adding Big Numbers.js
@@ -22,3 +22,17 @@ function add(a, b) {
 
   return sum;
 }
+
+// clever solution
+
+function add (a, b) {
+  var res = '', c = 0
+  a = a.split('')
+  b = b.split('')
+  while (a.length || b.length || c) {
+    c += ~~a.pop() + ~~b.pop()
+    res = c % 10 + res
+    c = c > 9
+  }
+  return res
+}
