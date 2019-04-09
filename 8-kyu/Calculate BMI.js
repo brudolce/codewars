88a20bcac1504a45356d4ab3521b36c0aeec9288
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-09 10:56:47 -0300

    done

---
+++ 8-kyu/Calculate BMI.js
@@ -0,0 +1,3 @@
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
