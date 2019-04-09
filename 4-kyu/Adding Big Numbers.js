60a5a5934b143764eaebe7afee2057b75c972b32
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-09 14:23:27 -0300

    done

---
+++ 4-kyu/Adding Big Numbers.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  var sum = "";
+  var carry = 0;
+  var partial;
+
+  for(let i=a.length-1, j=b.length-1; i>=0 || j>=0; i--, j--) {
+
+    partial = carry;
+    if(i >= 0) { partial += parseInt(a[i]); }
+    if(j >= 0) { partial += parseInt(b[j]); }
+
+    carry = 0;
+    
+    if(partial >= 10) {
+      carry = 1;
+      partial = partial - 10;
+    }
+    sum = partial.toString() + sum;
+  }
+
+  if(carry) { sum = '1' + sum; }
+
+  return sum;
+}
