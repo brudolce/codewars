12dfeba9d88c33966f9b701d926ea4172c67f9b1
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-10 11:04:50 -0300

    added alternative solution

--- 6-kyu/longest_palindrome.js
+++ 6-kyu/longest_palindrome.js
@@ -1,3 +1,14 @@
+const longestPalindrome = s =>{
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      var check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
+}
+
+//long solution
 longestPalindrome=function(s){
   var longest = 0;
   var length = s.length;
