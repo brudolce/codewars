8db9af56186ad96abe73886ee1dea3c59aa9ca7d
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-13 19:44:54 -0300

    added extra solution

--- 7-kyu/List to Array.js
+++ 7-kyu/List to Array.js
@@ -1,1 +1,10 @@
 const listToArray = list =>  list === null ? [] : [list.value].concat(listToArray(list.next))
+
+
+// solution #2
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.next)
+    array.push(node.value);
+  return array;
+}
