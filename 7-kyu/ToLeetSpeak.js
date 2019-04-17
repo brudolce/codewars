d4da23ee6e15da26370caf7df87769c1627b37f1
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-17 18:02:40 -0300

    meeh

---
+++ 7-kyu/ToLeetSpeak.js
@@ -0,0 +1,36 @@
+function toLeetSpeak(str) {
+  let result = ''
+  const data = {
+  A : '@',
+  B : '8',
+  C : '(',
+  D : 'D',
+  E : '3',
+  F : 'F',
+  G : '6',
+  H : '#',
+  I : '!',
+  J : 'J',
+  K : 'K',
+  L : '1',
+  M : 'M',
+  N : 'N',
+  O : '0',
+  P : 'P',
+  Q : 'Q',
+  R : 'R',
+  S : '$',
+  T : '7',
+  U : 'U',
+  V : 'V',
+  W : 'W',
+  X : 'X',
+  Y : 'Y',
+  Z : '2',
+  ' ':' '
+}
+  for (let e of str) {
+  result += data[e]   
+  }
+  return result;
+}
