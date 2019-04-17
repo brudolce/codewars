b813d7f5223728227498869950769cb39f7f9662
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-17 13:00:02 -0300

    clever solution added

--- 7-kyu/Return substring instance count.js
+++ 7-kyu/Return substring instance count.js
@@ -1,3 +1,10 @@
+//russian cool solution
 function solution(fullText, searchText){
   return (fullText.match(new RegExp(searchText, 'g')) || []).length;
+}
+
+
+//clever
+function solution(fullText, searchText){
+  return fullText.split(searchText).length - 1;
 }
