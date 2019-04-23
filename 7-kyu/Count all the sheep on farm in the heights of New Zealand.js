765a33a10b6af4945eed752021f8d7127ed8655e
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-23 13:18:59 -0300

    added clever solution

--- 7-kyu/Count all the sheep on farm in the heights of New Zealand.js
+++ 7-kyu/Count all the sheep on farm in the heights of New Zealand.js
@@ -1,3 +1,8 @@
 function lostSheep(friday,saturday,total){
   return total-(friday.reduce((acc,next)=>acc+next,0))-(saturday.reduce((acc,next)=>acc+next,0))
 }
+
+//clever solution
+function lostSheep(friday,saturday,total){
+  return friday.concat(saturday).reduce((s,l)=>s-l,total)
+}
