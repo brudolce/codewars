e1f38a39a39f4f573d29a75f46b0aa8052ca2d1e
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-23 21:29:57 -0300

    done

--- 7-kyu/Alphabet war.js
+++ 7-kyu/Alphabet war.js
@@ -14,3 +14,10 @@ function alphabetWar(fight) {
      right < left ? 'Right side wins!' :
      'Left side wins!'
 }
+
+//clever stuff
+function alphabetWar(fight) {
+    let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 };
+    let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0);
+    return result ? (result < 0 ? "Left" : "Right") + " side wins!" : "Let's fight again!";
+}
