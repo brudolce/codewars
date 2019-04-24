aa8a0b4f85248d6c803341257e189033325ceecb
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-24 18:11:18 -0300

    ok

---
+++ 8-kyu/Grader.js
@@ -0,0 +1,7 @@
+function grader(score) {
+  if (score > 1 || score < 0.6) return 'F'
+  if (score >= 0.9) return 'A'
+  if (score >= 0.8) return 'B'
+  if (score >= 0.7) return 'C'
+  if (score >= 0.6) return 'D'
+}
