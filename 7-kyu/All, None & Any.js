fc12a500d10d0cda25c09ee6889936df600cb0e1
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-26 21:21:08 -0300

    done

---
+++ 7-kyu/All, None & Any.js
@@ -0,0 +1,11 @@
+Array.prototype.all = function (p) {
+  return this.filter(p).length === this.length;
+};
+
+Array.prototype.none = function (p) {
+  return this.filter(p).length === 0;
+};
+
+Array.prototype.any = function (p) {
+  return this.filter(p).length > 0;
+};
