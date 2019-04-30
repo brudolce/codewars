6ad09402ca1f9fbe02d66cf4b704191accb930f2
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-29 21:11:16 -0300

    done

---
+++ 6-kyu/makeBackronym.js
@@ -0,0 +1,35 @@
+const makeBackronym = s => {
+
+const data = {
+  A: 'awesome',
+  B: 'beautiful',
+  C: 'confident',
+  D: 'disturbing',
+  E: 'eager',
+  F: 'fantastic',
+  G: 'gregarious',
+  H: 'hippy',
+  I: 'ingestable',
+  J: 'joke',
+  K: 'klingon',
+  L: 'literal',
+  M: 'mustache',
+  N: 'newtonian',
+  O: 'oscillating',
+  P: 'perfect',
+  Q: 'queen',
+  R: 'rant',
+  S: 'stylish',
+  T: 'turn',
+  U: 'underlying',
+  V: 'volcano',
+  W: 'weird',
+  X: 'xylophone',
+  Y: 'yogic',
+  Z: 'zero',
+}
+
+ return s.toUpperCase().split('')
+  .reduce((backronym, char) => `${backronym} ${dict[char]}`, '')
+  .slice(1)
+}
