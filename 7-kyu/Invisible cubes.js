e4913a5d775d9612b06b44ee1ad8a400647e5dd5
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-30 16:30:34 -0300

    added clever solution

--- 7-kyu/Invisible cubes.js
+++ 7-kyu/Invisible cubes.js
@@ -1,3 +1,7 @@
+//took me almost 45 minutes to come out with this
 function notVisibleCubes(n) {
   return n>3 ? (n-2) * (n-2) * (n-2) : n==3 ? 1 : 0
 }
+
+//some math genious worte this
+const notVisibleCubes = n => Math.pow(Math.max(n - 2, 0), 3);
