bc0310f6074e99e0685e6195a20d856e6c09669d
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-30 15:56:47 -0300

    added clever solution

--- 7-kyu/The old switcheroo 2.js
+++ 7-kyu/The old switcheroo 2.js
@@ -1,3 +1,11 @@
 function encode(str) {
   return str.split('').map(v=>/[a-z]/gi.test(v)?v.toLowerCase().charCodeAt()-96:v).join('')
 }
+
+//clever solution
+const encode = str => {
+  return str.replace(/[a-z]/gi, l => {
+    let cc = l.charCodeAt(0);
+    return cc - (cc < 97 ? 64 : 96);
+  });
+}
