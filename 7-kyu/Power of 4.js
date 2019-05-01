796d7353511f1bc684a978b19d88d59b7fa6cec0
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-01 16:49:48 -0300

    added clever solution

--- 7-kyu/Power of 4.js
+++ 7-kyu/Power of 4.js
@@ -1,3 +1,11 @@
+//regular dude solution
+function powerOf4(n) {
+    if (n < 4) return false;
+    while (n >= 4) n /= 4;
+    return n === 1;  
+}
+
+//smart ass math solution
 function powerOf4(n) {
   return n > 1 && typeof n === 'number' 
     ? Number.isInteger(Math.log(n)/Math.log(4))
