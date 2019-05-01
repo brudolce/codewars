14eaa093bb6ee9bfb8e010796b31575eeedec313
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-30 21:52:21 -0300

    ok

---
+++ 6-kyu/Calculate String Rotation.js
@@ -0,0 +1,14 @@
+function shiftedDiff(first,second){
+  if (first == second) return 0;
+  if (first.length != second.length) return -1;
+  let comp = first[first.length-1] + first.substring(0, first.length-1);
+  let i = 1;
+  while (comp != first) {
+    if (comp == second) return i;
+    comp = comp[comp.length-1] + comp.substring(0, comp.length-1);
+    i += 1;
+  }
+  return -1;
+}
+
+//clever stuff
