a7de733d3252055eadbd2e576a388a22c658acbf
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-04-30 21:52:45 -0300

    added clever solution

--- 6-kyu/Calculate String Rotation.js
+++ 6-kyu/Calculate String Rotation.js
@@ -12,3 +12,7 @@ function shiftedDiff(first,second){
 }
 
 //clever stuff
+function shiftedDiff(first, second) {
+  if (first.length != second.length) return -1
+  return (second + second).indexOf(first)
+}
