fd54ecc786dcb700e91023785bb9ebe60bc3ab04
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-02 12:58:33 -0300

    added clever solution

--- 6-kyu/Financing Plan on Planet XY140Z-n.js
+++ 6-kyu/Financing Plan on Planet XY140Z-n.js
@@ -1,7 +1,10 @@
 function finance(n) {
   let acc = 0, el;
-	for (let i = 0; i <= n; i ++){ 
+  for (let i = 0; i <= n; i ++){ 
 		el = (2 * i + 2 * i + (n - i)) * (n + 1 - i) / 2
 		acc += el}
 	return acc;
 }
+
+//clever solution
+const finance = (n) => n * (n + 1) * (n + 2) / 2;
