e5352677aa36713e6b90a8be0c41c55add670369
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-05 09:53:21 -0300

    added clever solution

--- 7-kyu/Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product.js
+++ 7-kyu/Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product.js
@@ -1,3 +1,11 @@
+//clever solution
+function product(s){
+  excs = s.split('!').length - 1
+  ques = s.split('?').length - 1
+  return excs * ques
+}
+
+//RegEx solution
 function product(s){
   return (s.match(/\?/g)||[]).length*(s.match(/!/g)||[]).length
 }
