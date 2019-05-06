23bec64725c4210371ab7f46a9d52e78257e912a
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-06 16:41:41 -0300

    added clever solution

---
+++ 4-kyu/Square into Squares. Protect trees!.js
@@ -0,0 +1,29 @@
+function decompose(n) {
+    let result = decomposer(n, n * n);
+    return result == null ? null : result.slice(0, result.length - 1);
+}
+
+function decomposer(n, remain){
+  if(remain == 0) {  
+    return [n]; 
+  }
+  
+  for(let i = n - 1; i > 0; i--){
+    if((remain - i * i) >= 0){
+      let r = decomposer(i, (remain - i * i));
+      
+      if(r != null){
+        r.push(n);
+        return r;
+      }
+    }
+  }
+
+  return null;
+}
+
+//clever solution
+function decompose(n, n2=n*n, i=n, prev) {
+  while(n2>0 && i-->1) if (prev = decompose(n, n2-i*i, i)) return prev.concat([i]);
+  return (n2 == 0) ? [] : null;
+}
