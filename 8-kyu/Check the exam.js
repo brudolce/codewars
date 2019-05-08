85a2d0cadd8a288f96f4d3c697515644c2bf93b3
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-08 16:51:35 -0300

    done

--- 8-kyu/Check the exam.js
+++ 8-kyu/Check the exam.js
@@ -1,4 +1,1 @@
-const checkExam = (a,b) => {
-  let r =b.reduce((acc,e,i)=> acc + (e===''?0:a[i]===b[i]?4:-1) ,0)
-  return r < 0 ? 0 : r
-}
+const checkExam = (a,b) => (r =b.reduce((acc,e,i)=> acc + (e===''?0:a[i]===b[i]?4:-1) ,0)) < 0 ? 0 : r
