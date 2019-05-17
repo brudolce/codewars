902f0402974a59330e5d94ac530cea3ea05e0578
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-17 11:42:28 +0200

    done

---
+++ 8-kyu/For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre.js
@@ -0,0 +1,4 @@
+const quote = fighter =>
+  fighter.toLowerCase() === 'george saint pierre'
+    ? 'I am not impressed by your performance.'
+    : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
