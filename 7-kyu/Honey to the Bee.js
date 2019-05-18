15bcdea8d2362ec8645696d89956b2b0218eb2f3
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-18 21:39:17 +0200

    done

---
+++ 7-kyu/Honey to the Bee.js
@@ -0,0 +1,20 @@
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+
+function Hive() {
+  this.pollen = 100;
+}
+
+Hive.prototype.addPollen = function(pollen) {
+  this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+  return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+  this.hive.addPollen(this.capacity);
+}
