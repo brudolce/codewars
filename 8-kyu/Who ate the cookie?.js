7784dbcb2824d38ea71070ea897def3d4493289b
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-21 20:34:44 +0200

    ok

---
+++ 8-kyu/Who ate the cookie?.js
@@ -0,0 +1,3 @@
+function cookie(x){
+  return typeof x == "string" ? "Who ate the last cookie? It was Zach!" : typeof x == "number" ? "Who ate the last cookie? It was Monica!" : "Who ate the last cookie? It was the dog!";
+}
