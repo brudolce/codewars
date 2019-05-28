c60d63934854da3e23232a7dce6df3a01a1bc8a8
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-05-28 11:46:38 +0200

    ok

---
+++ 8-kyu/Grasshopper - Terminal Game #1.js
@@ -0,0 +1,16 @@
+
+class Hero {
+  constructor(
+    name = 'Hero',
+    position = '00',
+    health = 100,
+    damage = 5,
+    experience = 0
+  ) {
+    this.name = name
+    this.position = position
+    this.health = health
+    this.damage = damage
+    this.experience = experience
+  }
+}
