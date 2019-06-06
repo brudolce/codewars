39f2c3b15d70d53de7f7b37855f988128fa4be6a
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-06-06 09:42:43 +0200

    ok

--- 6-kyu/Alphabetized.js
+++ 6-kyu/Alphabetized.js
@@ -1,5 +1,6 @@
 function alphabetized(s) {
-  return s.replace(/[^a-b]/ig, '').split('').map((a, i) => [a, i])
-  .sort((a, b) => a[0].toLowerCase().charCodeAt(0) > b[0].toLowerCase().charCodeAt(0) ? 1 : a[0].toLowerCase().charCodeAt(0) < b[0].toLowerCase().charCodeAt(0) ? -1 : a[1] - b[1])
-  .map(a => a[0]).join("");
+  s = s.replace(/[^A-Z]/ig, '').split('').map((a, i) => [a, i])
+  .sort((a, b) => a[0].toLowerCase().charCodeAt(0) > b[0].toLowerCase().charCodeAt(0) ? 1 : a[0].toLowerCase().charCodeAt(0) < b[0].toLowerCase().charCodeAt(0) ? -1 : a[1] - b[1]);
+  
+  return s.map(a => a[0]).join("");
 }
