cf8a788ed9845502ab19b4c953e6f0de57f459c5
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-06-09 16:24:25 +0200

    added clever solution

--- 4-kyu/Magnet particules in boxes.js
+++ 4-kyu/Magnet particules in boxes.js
@@ -7,3 +7,15 @@ function doubles(maxk, maxn) {
     }
     return s;
 }
+
+//clever solution
+function doubles(maxk, maxn) {
+  var s = 0;
+  for (var k = 1; k <= maxk; ++k)
+  for (var n = 1; n <= maxn; ++n) s += v(k, n);
+  return s;
+}
+
+function v(k, n) {
+  return 1/(k * Math.pow((n + 1), 2*k));
+}
