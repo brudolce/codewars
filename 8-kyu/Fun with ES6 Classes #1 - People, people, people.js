89545d0c9a98d27b6c2b74bc0090fa19f3f0f281
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-06-13 21:11:29 +0200

    done

---
+++ 8-kyu/Fun with ES6 Classes #1 - People, people, people.js
@@ -0,0 +1,11 @@
+class Person {
+  constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+    Object.assign(this, { firstName, lastName, age, gender });
+  }
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
