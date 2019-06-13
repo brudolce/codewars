76f9e6f6bf21ae6120140cb1e766bde15e380e51
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-06-13 21:15:25 +0200

    done

---
+++ 8-kyu/Crash Override.js
@@ -0,0 +1,8 @@
+const firstLetter = str => str[0].toUpperCase()
+
+const isValidName = name => /[a-z]/gi.test(name)
+
+const aliasGen = (fName, sName) =>
+  isValidName(fName[0]) && isValidName(sName[0])
+    ? `${firstName[firstLetter(fName)]} ${surname[firstLetter(sName)]}`
+    : 'Your name must start with a letter from A - Z.'
