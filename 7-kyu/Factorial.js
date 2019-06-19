1be8d4d7a678098d05bfebc7d75593b21db906ce
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-06-19 09:30:39 +0200

    added extra solution

--- 7-kyu/Factorial.js
+++ 7-kyu/Factorial.js
@@ -2,3 +2,12 @@ function factorial(n) {
   if (n < 0 || n > 12) throw new RangeError('Range must be between 0 and 12')
   return n>1?n*factorial(n-1):1;
 }
+
+//sol 2
+const factorial = n =>
+  n >= 0
+    ? Array.from({ length: n }, (_, index) => index + 1).reduce(
+        (total, value) => total * value,
+        1
+      )
+    : null
