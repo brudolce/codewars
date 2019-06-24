5d58ed3a96188de7f04982885226beaea48421b6
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-06-24 17:34:34 +0200

    done

---
+++ 7-kyu/HTML Generator.js
@@ -0,0 +1,10 @@
+function HTMLGen () {
+  this.div=s=>"<div>"+s+"</div>";
+  this.b=s=>"<b>"+s+"</b>";
+  this.p=s=>"<p>"+s+"</p>";
+  this.a=s=>"<a>"+s+"</a>";
+  this.body=s=>"<body>"+s+"</body>";
+  this.span=s=>"<span>"+s+"</span>";
+  this.title=s=>"<title>"+s+"</title>";
+  this.comment=s=>"<!--"+s+"-->";
+}
