f99f166d48b844b516511b0b0f4cdac2cb67fc97
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-09-13 11:22:05 +0200

    done

--- 6-kyu/Circularly Sorted Array.js
+++ 6-kyu/Circularly Sorted Array.js
@@ -1,4 +1,5 @@
 function isCircleSorted( arr ){
+    if (arr.length ===0) return true
     let cnt = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > arr[(i + 1) % arr.length]) {
