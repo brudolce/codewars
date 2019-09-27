2f46f9f599fd2d41c49b6ca21e0b6c05a66f7260
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-09-27 21:50:02 +0200

    done

---
+++ 6-kyu/Vasya - Clerk.java
@@ -0,0 +1,25 @@
+public class Line {
+  public static String Tickets(int[] peopleInLine)
+  {
+        int d25 = 0;
+        int d50 = 0;
+        for (int i = 0; i < peopleInLine.length; i++) {
+            if (peopleInLine[i] == 25) d25++;
+            if (peopleInLine[i] == 50) {
+                d25--;
+                d50++;
+            }
+            if (peopleInLine[i] == 100) {
+                if (d50 > 0) {
+                    d50--;
+                    d25--;
+                } else {
+                    d25 -= 3;
+                }
+            }
+            if (d25 < 0) return "NO";
+        }
+        return "YES";
+  }
+}
+
