a59e718750c6b9bbb4929c7ea84bdb91ef269578
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-09-29 14:57:38 +0200

    oke

---
+++ 7-kyu/Descending Order.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class DescendingOrder {
+  public static int sortDesc(final int num) {
+        char[] tempArray = String.valueOf(num).toCharArray();
+        Arrays.sort(tempArray);
+        return Integer.parseInt(new StringBuilder(new String(tempArray)).reverse().toString());
+    }
+}
