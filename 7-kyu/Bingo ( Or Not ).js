5e6e8182abaaa5142761f4f1406baf8aab96d583
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2019-12-02 14:18:56 +0100

    added solution

--- 7-kyu/Bingo ( Or Not ).js
+++ 7-kyu/Bingo ( Or Not ).js
@@ -2,3 +2,6 @@ function bingo(a) {
   let f = new Set(a.filter(x=>x===2||x===9||x===14||x===7||x===15))
   return Array.from(f).length === 5 ? 'WIN' : 'LOSE'
 }
+
+//solution 2
+const bingo = ar => [2,7,9,14,15].every(e => ar.includes(e)) ? 'WIN' : 'LOSE'
