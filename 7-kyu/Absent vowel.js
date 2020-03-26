9364e0865dddae0ff31cbf526c8ad2efca6a467d
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2020-03-26 20:38:02 +0100

    done

---
+++ 7-kyu/Absent vowel.js
@@ -0,0 +1,7 @@
+function absentVowel(x) {
+  if (!x.match(/a/)) return 0;
+  if (!x.match(/e/)) return 1;
+  if (!x.match(/i/)) return 2;
+  if (!x.match(/o/)) return 3;
+  if (!x.match(/u/)) return 4;
+}
