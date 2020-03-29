3d181d8e95d7a43b21fbe9663ee746cf29db21d9
Bruno Dolce <45857814+brudolce@users.noreply.github.com>
2020-03-29 17:19:27 +0200

    done

---
+++ 6-kyu/Parabolic Arc Length.js
@@ -0,0 +1,12 @@
+function lenCurve(n) {
+  const f = x => x*x
+  
+  let res = 0;
+  let h = 1 / n;
+  for (let i = 0; i < 1; i += h) {
+    res += Math.sqrt(Math.pow(h, 2) + Math.pow(f(i) - f(i + h), 2));
+    n -= 1;
+    if (n === 0) break;
+  }
+  return parseFloat(res.toFixed(9));
+}
